e63d1078b9e7ad5b5a78170319b5f1a62e83825e
Joshua Needham <joshuabneedham@gmail.com>
2017-09-13 20:08:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   for(var i = 0; musicians.length < 0 && instruments.length < 0; i++) {
     plays.push(musicians + ' plays ' + instruments);
   }
+  return plays;
 }
 function johnLennonFacts(arg) {
   var newFacts = [];
